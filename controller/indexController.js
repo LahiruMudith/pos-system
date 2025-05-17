31dcd244de5650074641a5790cc807fe340bb148
Lahiru_Mudith <lahimudith@gmail.com>
2025-05-17 13:49:02 +0530

    responsive all pages

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,6 +1,7 @@
-import {customer_db, item_db} from "../db/db.js";
+import {customer_db, item_db, order_db} from "../db/db.js";
 import CustomerModel from "../model/customerModel.js";
 import ItemModel from "../model/itemModel.js";
+import OrderModel from "../model/OrderModel.js";
 
 let customer = new CustomerModel('C001', 'Lahiru', 'Muditha', '0777777777', 'Colombo');
 customer_db.push(customer);
@@ -15,6 +16,15 @@ item_db.push(item2);
 
 var cart = [];
 var total = 0;
+$("#discount").val(0);
+
+// loadId()
+
+// function loadId(){
+//     console.log(order_db);
+//     let lastOrderId = order_db[order_db.length - 1];
+//     console.log(lastOrderId);
+// }
 
 $('#btnSearchCustomer').on('click', function(event) {
     event.preventDefault();
@@ -97,6 +107,7 @@ $('#btnAddToCart').on('click', function (event){
     let qty = $('#qty').val();
     let price = $('#price').val();
     let orderQty = $('#orderQty').val();
+    let totalPrice = Number(price) * Number(orderQty);
 
     let isValidItem = true;
     item_db.forEach(function (item){
@@ -118,7 +129,7 @@ $('#btnAddToCart').on('click', function (event){
         return;
     }
 
-    let item = {itemId, itemName,qty, price, orderQty};
+    let item = {itemId, itemName, price, orderQty, totalPrice};
     let isNewItem = true;
     cart.forEach(function (cart){
         if (cart.itemId === itemId){
@@ -129,18 +140,17 @@ $('#btnAddToCart').on('click', function (event){
     if (isNewItem){
         cart.push(item);
     }
-    console.log(cart);
 
     $('#table-body').empty();
     total = 0;
     cart.forEach(function (cart){
-        let itemData = `<tr>
-                            <td>${cart.itemId}</td>
-                            <td>${cart.itemName}</td>
-                            <td>${cart.qty}</td>
-                            <td>${cart.price}</td>
-                            <td>${cart.orderQty}</td>
-                            <td><button type="button" class="btn btn-danger">Delete</button></td>
+        let itemData = `<tr class="row">
+                            <td class="col">${cart.itemId}</td>
+                            <td class="col">${cart.itemName}</td>
+                            <td class="col">${cart.orderQty}</td>
+                            <td class="col">${cart.price}</td>
+                            <td class="col">${cart.totalPrice}</td>
+                            <td class="col"><button type="button" class="btn btn-danger">Delete</button></td>
                         </tr>`
 
         $('#table-body').append(itemData);
@@ -148,5 +158,83 @@ $('#btnAddToCart').on('click', function (event){
         total = Number(total) + ((Number(cart.price) * Number(cart.orderQty)));
     });
     $('#lblTotal').text(`TOTAL : ${total} /=`);
+    console.log(total)
+});
+
+$('#btn-place-order').on('click', function (event){
+    event.preventDefault();
+
+    if ($('#firstName').val() === "") {
+        Swal.fire({
+            title: "Please select a customer",
+            icon: "error",
+            timer: 1000
+        });
+        return;
+    }
+    if (cart.length === 0){
+        Swal.fire({
+            title: "Please add items to cart",
+            icon: "error",
+            timer: 1000
+        });
+        return;
+    }
+    if ($('#cash').val() === 0 || $('#cash').val() === "") {
+        Swal.fire({
+            title: "Please enter cash",
+            icon: "error",
+            timer: 1000
+        });
+        return;
+    }
+    let customerId = $('#customerId').val();
+    let customerName = $('#firstName').val() + " " + $('#lastName').val();
+    let phoneNumber = $('#phoneNumber').val();
+    let orderDate = new Date().toLocaleDateString();
+    let orderId = $('#orderId').val();
+    let cash = $('#cash').val();
+    let balance = $('#balance').val();
+    let discount = $('#discount').val();
+    let items = [];
+    cart.forEach(function (cart){
+        let item = {
+            itemId: cart.itemId,
+            itemName: cart.itemName,
+            qty: cart.orderQty,
+            price: cart.price,
+            totalPrice : Number(cart.price) * Number(cart.orderQty)
+        }
+        items.push(item);
+    });
+    // if (Number(cash) <= total) {
+    //     Swal.fire({
+    //         title: "Enter Valid Cash Amount",
+    //         icon: "error",
+    //         timer: 1000
+    //     });
+    //     return;
+    // }
+    let order = new OrderModel(orderId, orderDate, customerId, customerName, phoneNumber, total,cash, balance, discount, items);
+    order_db.push(order);
+    Swal.fire({
+        title: "Done",
+        icon: "success",
+        timer: 1000
+    });
+    console.log(order_db);
+});
+
+$("#cash").on('keyup', function (event){
+    let discount = $("#discount").val();
+    let cash  = $("#cash").val();
+    let balance = 0;
+    let discountPrice = discount * total / 100;
+
+
+    console.log(total);
+    if (total <= Number(cash)) {
+        $("#balance").val(Number(cash) - (total - discountPrice));
+    }
 
 });
